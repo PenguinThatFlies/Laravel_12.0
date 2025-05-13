b965309aa7208c6825baf6487df8e5fb354d05a0
I.S <127890770+PenguinThatFlies@users.noreply.github.com>
2025-05-13 17:01:14 +0400

    layout.blade.php

---
+++ CRUD/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ka">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel პროდუქტი</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 text-gray-800">
+
+<div class="container mx-auto px-4 py-6">
+    {{ $slot }}
+</div>
+
+</body>
+</html>
